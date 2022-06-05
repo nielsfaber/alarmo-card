25af44f259d5c33661e29836e37c427f4f10c059
neliss <niels.r.faber@gmail.com>
2022-06-05 20:30:19 +0200

    release v1.4.1

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 import { CardConfig } from './types';
 
-export const CARD_VERSION = 'v1.4.0';
+export const CARD_VERSION = 'v1.4.1';
 
 export const BUTTONS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '', '0', 'clear'];
 
